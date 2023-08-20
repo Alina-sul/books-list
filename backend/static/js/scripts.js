a00857ab325f8eeab36faa8f81d26801a296f302
Alina-sul <alina.sulkovsky@gmail.com>
2023-08-20 08:44:02 +0300

    removed unused script file

--- backend/static/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function searchBooks() {
-    console.trace("searchBooks() called");
-    const searchInput = document.getElementById("search-input");
-    const searchTerm = searchInput.value;
-    const booksListDiv = document.querySelector(".books-list");
-    booksListDiv.setAttribute("hx-get", `/api/books-htmx/?search=${searchTerm}`);
-    htmx.trigger(booksListDiv, 'htmx:send');
-}
